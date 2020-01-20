f01d969b06ab48a3e062963f36e20fbff3cf1814
LegendSeal21 <60075464+LegendSeal21@users.noreply.github.com>
2020-01-20 12:51:10 -0800

    Turret gives player dual wield

---
+++ UnityGames/Bot Shooter/Assets/FPS/Scripts/GiveDualWield.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GiveDualWield : MonoBehaviour
+{
+    void OnDestroy()
+    {
+        GivePlayerDualWield();
+    }
+
+    public void GivePlayerDualWield()
+    {
+        foreach (var wpnMgr in GameObject.FindObjectsOfType<PlayerWeaponsManager>())
+        {
+            if (wpnMgr != null && !wpnMgr.isActiveAndEnabled)
+            {
+                wpnMgr.enabled = true;
+                Debug.Log("GIVING DUAL WIELD 2 PLAYA!");
+            }
+        }
+    }
+}
